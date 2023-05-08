bcbab444bfb4773ec0ceef6997f67761f1bc127a
Dexytron <deanpolimac@yahoo.com>
2023-05-08 18:07:37 +0200

    Set Up Server Repo
    In main.py a simple function is added, as well a test using pytest for it in simple_test.py. The requirements.txt and pyproject.toml resemble the files on the client side.

---
+++ main.py
@@ -0,0 +1,6 @@
+def print_hi(name):
+    return f'Hello, {name}'
+
+
+if __name__ == '__main__':
+    print_hi('world')
---
+++ simple_test.py
@@ -0,0 +1,5 @@
+from main import print_hi
+
+
+def test():
+    assert print_hi('world') == 'Hello, world'
