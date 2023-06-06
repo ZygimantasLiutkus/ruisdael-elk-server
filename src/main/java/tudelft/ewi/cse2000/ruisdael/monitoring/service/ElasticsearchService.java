cc2206472da47dc00790b36e2313eb6ff8eaa1ca
Cas Ebels <c.n.ebels@student.tudelft.nl>
2023-06-06 17:08:03 +0200

    Update templates from refactor

--- src/main/java/tudelft/ewi/cse2000/ruisdael/monitoring/service/ElasticsearchService.java
+++ src/main/java/tudelft/ewi/cse2000/ruisdael/monitoring/service/ElasticsearchService.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -72,6 +73,7 @@ public class ElasticsearchService {
 
         return distinctIndexNames.stream()
                 .map(this::getDeviceDetailsFromName)
+                .filter(Objects::nonNull)
                 .toList();
     }
 
