4422fe1b379f33050a854fd17dbf527738a61c3f
Hor T <hor@Hors-MacBook-Air.local>
2023-06-25 17:54:44 +0200

    Navbar and missing files fixed.

---
+++ src/main/resources/static/js/alert-table.js
@@ -0,0 +1,46 @@
+/**
+ * Takes an array of alerts and gives back a html table of it.
+ */
+function createTable(alertsArray) {
+    alertList = (alertsArray == null) ? alerts : alertsArray;
+
+    let tbody = table.getElementsByTagName('tbody')[0];
+
+    while (tbody.rows.length > 0) {
+        tbody.deleteRow(0);
+    }
+
+
+    for (let i = 0; i < alertList.length; i++) {
+        let row = tbody.insertRow(tbody.rows.length);
+        let alert = alertList[i];
+
+        
+        let timeStamp = new Date(alert['timeStamp'] + 'Z');
+        row.insertCell(0).innerText = timeStamp.toLocaleTimeString() + " " + timeStamp.toLocaleDateString();
+        row.insertCell(1).innerText = alert['deviceName'];
+        row.insertCell(2).innerText = alert['metric'];
+
+        let oldFlag = alert['oldFlag'];
+        let newFlag = alert['newFlag'];
+
+        row.insertCell(3).innerText = oldFlag;
+        row.cells[3].style.color = row.cells[3].innerText;
+        
+
+        row.insertCell(4).innerText = newFlag;
+        row.cells[4].style.color = row.cells[4].innerText;
+
+        row.insertCell(5).innerText = 'Issue';
+
+        row.children[1].addEventListener("click", () => {
+            window.location.href = "/node/" + alert['deviceName'];
+        });
+
+        row.children[5].addEventListener("click", () => {
+            window.location.href = "GITLAB"
+        });
+
+        
+    }
+}
---
+++ src/main/resources/static/js/alerts.js
@@ -0,0 +1,41 @@
+const table = document.getElementById("alerts-table");
+
+let socket = new SockJS('/alert-update');
+let client = Stomp.over(socket);
+
+window.addEventListener("load", init);
+
+/**
+ * Function that connects to the websockets and sends messages to the '/app/alerts'
+ * message path with an interval set by the ApplicationConfig.
+ */
+function connect() {
+    client.connect({}, () => {
+        interval = setInterval(() => {
+            client.send('/app/alerts', {});
+        }, websocketDelay);
+
+        client.subscribe('/topic/alerts', (d) => {
+            allAlerts = JSON.parse(d.body);
+            createTable(allAlerts);
+        });
+    });
+}
+
+/**
+ * Function that disconnects from the websockets.
+ */
+function disconnect() {
+    client.disconnect()
+    interval = null;
+}
+
+// The initialize function sets up all the event listeners for the elements in the HTML page, and calls the
+// createTable() function, as well as the setUp() function which initializes the Pagination.
+function init() {
+    // Runs the `connect()` function on load to create a connection with the websockets
+    connect();
+    createTable(allAlerts);
+}
+
+
